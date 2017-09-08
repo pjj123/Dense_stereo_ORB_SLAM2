21217c271715fe43032b4846e3922362cb23c005
pjj123 <991301344@qq.com>
2017-09-08 16:13:07 +0800

    Create octomap-config.cmake

---
+++ cmake_modules/octomap-config.cmake
@@ -0,0 +1,4 @@
+set(OCTOMAP_INCLUDE_DIRS "~/zhumulu/octomap/octomap/include")
+set(OCTOMAP_LIBRARY_DIRS "~/zhumulu/octomap/lib")
+set(OCTOMAP_LIBRARIES "~/zhumulu/octomap/lib/liboctomap.so" "~/zhumulu/octomap/lib/liboctomath.so")
+)
